bd647ed697c2b940751bc91d48f27921b103a587
ahunna1232 <udondupufrancis@gmail.com>
2022-09-08 19:55:54 -0700

    task5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: it is going to always return 0
+ */
+int main(void)
+{
+	("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
